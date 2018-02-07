339af034a25fb239fd37c1af1db515e6323e1888
senix <senix86@gmail.com>
2018-02-07 22:12:43 +0600

    Upload

--- 1.JavaSyntax/src/com/javarush/task/task04/task0427/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0427/Solution.java
@@ -10,9 +10,7 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String sYear = reader.readLine();
-        int a = Integer.parseInt(sYear);
-
+        int a = Integer.parseInt(reader.readLine());
 
        String s1 = "четное однозначное число";
        String s2 = "нечетное однозначное число";
@@ -21,11 +19,23 @@ public class Solution {
        String s5 = "четное трехзначное число";
        String s6 = "нечетное трехзначное число";
 
-       //if ((a >= 1) & (a<=999)){
-           //if (a ){
+      if (a>0 && (a/10==0) && (a%2==0)){
+          System.out.println(s1);
 
-           }
-       }
+      }else if (a>0 && (a/10==0) && (a%2!=0)){
+          System.out.println(s2);
 
-   // }
-//}
+      }else if (a>0 && (a/100==0) && (a%2==0)){
+          System.out.println(s3);
+
+      }else if (a>0 && (a/100==0) && (a%2!=0)){
+          System.out.println(s4);
+
+      }else if (a>0 && (a/1000==0) && (a%2==0)){
+          System.out.println(s5);
+
+      }else if (a>0 && (a/1000==0) && (a%2!=0)){
+          System.out.println(s6);
+      }
+    }
+}
