40c30457cc4051e6f8a148939ca7b846e5ea0e2b
senix <senix86@gmail.com>
2018-02-07 20:14:26 +0600

    Upload

--- 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
@@ -9,5 +9,16 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double min = Double.parseDouble((reader.readLine()));
+        double result = (min % 5);
+
+        if (result < 3){
+            System.out.println("зеленый");
+        }else if (result >= 3 && result < 4){
+            System.out.println("жёлтый");
+        }else if (result >= 4){
+            System.out.println("красный");
+        }
     }
 }
