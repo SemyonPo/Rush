d735b68d966bfc829cf72a8688805a658b6cfe4e
senix <senix86@gmail.com>
2018-02-13 19:51:20 +0600

    another solve, without ArrayList

--- 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
@@ -13,20 +13,21 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List arr = new ArrayList<Integer>();
+        //List arr = new ArrayList<Integer>();
 
-        int a = 0;
+        int sum = 0;
         for (; true; )
         {
             int num = Integer.parseInt((reader.readLine()));
             if (num == -1)
                 break;
-            arr.add(num);
+            //arr.add(num);
+            sum += num;
         }
 
-        for (int i = 0; i < arr.size(); i++) {
-            a += (Integer) arr.get(i);
-        }
-        System.out.println(a - 1);
+        //for (int i = 0; i < arr.size(); i++) {
+        //    a += (Integer) arr.get(i);
+        //}
+        System.out.println(sum - 1);
     }
 }
