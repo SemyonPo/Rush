d14a075ef7e162aaba3f504830ad20f96ab9c695
senix <senix86@gmail.com>
2018-02-13 19:29:23 +0600

    Upload

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task04.task0442;
+
+
+/* 
+Суммирование
+*/
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        int a = 0;
+        for (; true; )
+        {
+            int num = Integer.parseInt((reader.readLine()));
+            if (num == -1)
+                break;
+            arr.add(num);
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            a += arr.get(i);
+        }
+        System.out.println(a - 1);
+    }
+}
