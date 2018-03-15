59ed811d01f232e164c9676a07995ada4a89c5b9
senix <senix86@gmail.com>
2018-03-15 18:13:47 +0600

    add another tasks

--- 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
@@ -14,6 +14,7 @@ public class Solution {
 
         ArrayList<String> list = new ArrayList<String>();
         list.add("роза"); //0
+        list.add("мера");
         list.add("лоза"); //1
         list.add("лира"); //2
         list = fix(list);
@@ -25,7 +26,17 @@ public class Solution {
 
     public static ArrayList<String> fix(ArrayList<String> list) {
         //напишите тут ваш код
-
-        return null;
+        for (int i = 0; i < list.size(); ) {
+            if (list.get(i).contains("р") && !list.get(i).contains("л")) {
+                list.remove(i);
+            } else i++;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).contains("л") && !list.get(i).contains("р")){
+                list.add(list.get(i));
+                break;
+            }
+        }
+        return list;
     }
 }
