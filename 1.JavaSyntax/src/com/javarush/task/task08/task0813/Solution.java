44c5487ac541371b38e412697a3ed3b1602b5686
senix <senix86@gmail.com>
2018-04-16 20:10:09 +0600

    add another tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //напишите тут ваш код
+        Set<String> set = new HashSet<>();
+        set.add("Леjgра");
+        set.add("Лира");
+        set.add("Лjhjfера");
+        set.add("Лра");
+        set.add("Лваера");
+        set.add("Леукрваа");
+        set.add("Леурваа");
+        set.add("Л8098ера");
+        set.add("Лерра");
+        set.add("Лермлбиа");
+        set.add("Леыра");
+        set.add("Лермиа");
+        set.add("Лер45на");
+        set.add("Ле78ра");
+        set.add("Л4ера");
+        set.add("Лекерора");
+        set.add("Леаоо3ра");
+        set.add("Лер324а");
+        set.add("Лертк4а");
+        set.add("Ле35нрра");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
